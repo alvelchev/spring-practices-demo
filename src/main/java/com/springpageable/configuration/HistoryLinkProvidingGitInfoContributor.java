a81d49a8e598464bfd725414ae54a2645216ef27
Alexander Velchev (EXT) <alexander.velchev_ext@bbraun.com>
2022-11-15 15:54:45 +0200

    extend git commit plugin to return link to the latest commit

---
+++ src/main/java/com/springpageable/configuration/HistoryLinkProvidingGitInfoContributor.java
@@ -0,0 +1,30 @@
+package com.springpageable.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.info.GitInfoContributor;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class HistoryLinkProvidingGitInfoContributor extends GitInfoContributor {
+
+  public static final String GITHUB_ADDRESS =
+      "https://github.com/alvelchev/spring-practices-demo/commit/";
+  public static final String KEY_FOR_LINK = "history";
+
+  @Autowired
+  public HistoryLinkProvidingGitInfoContributor(GitProperties properties) {
+    super(properties);
+  }
+
+  protected void postProcessContent(Map<String, Object> content) {
+    super.postProcessContent(content);
+
+    final String commitId = getProperties().getShortCommitId();
+    if (commitId != null) {
+      content.put(KEY_FOR_LINK, GITHUB_ADDRESS + commitId);
+    }
+  }
+}
