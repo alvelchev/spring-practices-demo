99d3fb73ebcb5d0c071974a783c848b9860d4e64
Alexander Velchev (EXT) <alexander.velchev_ext@bbraun.com>
2023-02-12 11:18:22 +0200

    resolve issues after bump spring commons version

--- src/main/java/com/springpageable/DeviceApplication.java
+++ src/main/java/com/springpageable/DeviceApplication.java
@@ -1,12 +1,9 @@
 package com.springpageable;
 
-import com.springpageable.configuration.SwaggerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.springpageable.repository"})
 public class DeviceApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/springpageable/configuration/ApplicationConfig.java
+++ src/main/java/com/springpageable/configuration/ApplicationConfig.java
@@ -5,9 +5,6 @@ import com.springpageable.model.mapper.FutureDeviceMapperImpl;
 import com.springpageable.model.mapper.UserMapper;
 import com.springpageable.model.mapper.UserMapperImpl;
 import com.springpageable.storage.CountryStorage;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -15,20 +12,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.util.List;
 
 @Configuration
 @ConditionalOnProperty(value = "springdoc.api-docs.enabled", havingValue = "true")
 @ConfigurationPropertiesScan("com.device.configuration")
 @AllArgsConstructor
 public class ApplicationConfig {
-
-  private final SwaggerProperties swaggerProperties;
-
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI().info(getInfo()).servers(getServers());
-  }
 
   @Bean
   public FutureDeviceMapper futureDeviceMapper() {
@@ -42,15 +31,5 @@ public class ApplicationConfig {
   @Bean
   public CountryStorage countryStorage() throws IOException {
     return new CountryStorage();
-  }
-
-  private Info getInfo() {
-    return new Info()
-        .title(swaggerProperties.getProperties().getTitle())
-        .description(swaggerProperties.getProperties().getDescription());
-  }
-
-  private List<Server> getServers() {
-    return List.of(new Server().url(swaggerProperties.getServerUrl()));
   }
 }
